a4d59f67ca322bd9a65e4226e670e74ce3b6c805
ivan <nepupalam@mail.ru>
2022-05-19 16:48:18 +0300

    комит 1 дня

---
+++ main.js
@@ -0,0 +1,17 @@
+const objectA = {
+    colour: 'green',
+    sweet: true,
+    weight: 30,
+    calories: 100,
+    ripe: true
+}
+const objectB = {
+    colour: 'green',
+    sweet: true,
+    weight: 30,
+    calories: 100,
+    ripe: true
+}
+objectB.colour=objectA;
+console.log(objectB)
+
